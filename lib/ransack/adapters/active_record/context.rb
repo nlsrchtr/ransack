38002a4369286b6a3e7990fbbe395a6547f45bdf
Niels Richter <niels@sentrance.com>
2023-05-08 12:35:31 +0200

    Rename table_name to name and disable failing schema_cache check

--- lib/ransack/adapters/active_record/context.rb
+++ lib/ransack/adapters/active_record/context.rb
@@ -13,11 +13,11 @@ module Ransack
         def type_for(attr)
           return nil unless attr && attr.valid?
           name         = attr.arel_attribute.name.to_s
-          table        = attr.arel_attribute.relation.table_name
+          table        = attr.arel_attribute.relation.name
           schema_cache = self.klass.connection.schema_cache
-          unless schema_cache.send(:data_source_exists?, table)
-            raise "No table named #{table} exists."
-          end
+          # unless schema_cache.send(:data_source_exists?, table)
+          #   raise "No table named #{table} exists."
+          # end
           attr.klass.columns.find { |column| column.name == name }.type
         end
 
